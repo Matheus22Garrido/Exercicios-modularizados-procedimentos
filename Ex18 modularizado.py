8351025d8df78eae6cffc0b342dbce23dc1b2257
Matheus22Garrido <theusgarrido22@gmail.com>
2026-03-24 13:47:08 +0000

    primeiro ex pronto

--- Ex18 modularizado.py
+++ Ex18 modularizado.py
@@ -1,17 +1,26 @@
 #declarar
 valor1: int = 0
 valor2: int = 0
-diferença: int = 0
 
-#inicio
-valor1 = int(input("digite seu primeiro número: "))
-valor2 = int(input("digite seu segundo número: "))
-if (valor1>valor2):
-    diferença = (valor1-valor2)
-    print ("essa é a diferença entre seus números:", diferença)
-elif(valor1<valor2):
-    diferença = (valor2-valor1)
-    print ("essa é a diferença entre seus números:", diferença)
-else:
-    print ("seus números são iguais")
-#fim
+def ordemDiferença ():
+    global valor1
+    global valor2
+    diferença: int = 0
+    if (valor1>valor2):
+        diferença = (valor1-valor2)
+        print ("Essa é a diferença entre seus números:", diferença)
+    elif(valor1<valor2):
+        diferença = (valor2-valor1)
+        print ("Essa é a diferença entre seus números:", diferença)
+    else:
+        print ("Seus números são iguais")
+    
+def main():
+    global valor1
+    global valor2
+    valor1 = int(input("Digite seu primeiro número: "))
+    valor2 = int(input("Digite seu segundo número: "))
+    ordemDiferença()
+
+if __name__ == "__main__":
+    main()     
